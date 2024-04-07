980a3a54071842de111dec1ff68b0156a329981a
MaksOn <maksimchernyaev22@gmail.com>
2024-04-08 00:01:44 +0300

    fix: some corrections and improvements

--- init/db_migration.sh
+++ init/db_migration.sh
@@ -1,10 +1,18 @@
-target_version=${VERSION:latest}
-for migration_file in $(ls test_migrations/*.sql | sort -V); do
-    version=$(basename $migration_file | cut -d'_' -f1)
-    if [ "$version" -gt "${target_version}" ]; then
-        break
+target_version=${VERSION:-latest}
+
+for migration_file in $(ls migrations/*.sql | sort -V); do
+
+    if [[ "$target_version" != "latest" ]]; then
+
+        version=$(basename $migration_file | cut -d'_' -f1)
+
+        if [[ "$version" > "${target_version}" ]]; then
+            break
+        fi
     fi
+
     echo "Applying migration: $migration_file"
     psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f $migration_file
 done
-echo "Migration complete. Version: ${target_version}"
+
+echo "Migration complete. Current version: $target_version."
